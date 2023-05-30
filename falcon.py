62aa1e2233f57cf1de22405f588422129610a43b
Aleksey Korshuk <ale-kor02@mail.ru>
2023-05-29 21:11:30 -0700

    updates

--- falcon.py
+++ falcon.py
@@ -8,17 +8,18 @@ ds = load_dataset("AlekseyKorshuk/character-prepared-seeds", split="train")
 # set the default language model used to execute guidance programs
 # guidance.llm = guidance.llms.transformers.MPT("mosaicml/mpt-7b-storywriter", device=0)
 model = transformers.AutoModelForCausalLM.from_pretrained(
-    "wbrown/cassandra-6.9b",
-    torch_dtype=torch.float16,
+    "tiiuae/falcon-7b",
+    torch_dtype=torch.bfloat16,
     device_map="sequential",
     low_cpu_mem_usage=True,
     trust_remote_code=True,
 ).eval()
 # setattr(model.config, "max_position_embeddings", 2048)
 tokenizer = transformers.AutoTokenizer.from_pretrained(
-    "wbrown/cassandra-6.9b",
+    "tiiuae/falcon-7b",
     use_fast=False,
     trust_remote_code=True,
+    return_token_type_ids=False
 )
 # tokenizer.bos_token_id = 1
 # tokenizer.eos_token_id = 2
